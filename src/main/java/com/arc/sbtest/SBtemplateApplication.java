d5d1ca2e39a614636d702edee418ff5a8222a024
Anantha Raju C <arcswdev@gmail.com>
2018-08-06 20:41:59 +0530

    initial commit

---
+++ src/main/java/com/arc/sbtest/SBtemplateApplication.java
@@ -0,0 +1,12 @@
+package com.arc.sbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBtemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBtemplateApplication.class, args);
+	}
+}
