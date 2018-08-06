58d52bfde890c6276fd0c6325a2e6fb0d4139cdb
Anantha Raju C <arcswdev@gmail.com>
2018-08-06 22:27:53 +0530

    parameterized controller added

--- src/main/java/com/arc/sbtest/controller/MScontroller.java
+++ src/main/java/com/arc/sbtest/controller/MScontroller.java
@@ -2,15 +2,22 @@ package com.arc.sbtest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class MScontroller 
 {
-	@GetMapping("/microservice")
-	public String getAllNotes() 
+	@GetMapping("/generic-hello")
+	public String noReqestParameter() 
 	{
-	    return "micro service controller";
+	    return "hello from micro service controller";
+	}
+	
+	@GetMapping("/personalized-hello")
+	public String singleReqestParameter(@RequestParam(name="name", required=false, defaultValue="World") String name) 
+	{
+	    return "hello "+name;
 	}
 }
