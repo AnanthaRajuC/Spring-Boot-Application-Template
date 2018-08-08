107a4e7a545f0dc84ce8cf5a8c2c6f7cfa46db0e
Anantha Raju C <arcswdev@gmail.com>
2018-08-08 08:37:28 +0530

    GET POST PUT added

--- src/main/java/com/arc/sbtest/controller/MScontroller.java
+++ src/main/java/com/arc/sbtest/controller/MScontroller.java
@@ -1,20 +1,63 @@
 package com.arc.sbtest.controller;
 
+import java.util.List;
+import javax.validation.Valid;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.arc.sbtest.model.Person;
+import com.arc.sbtest.repository.PersonRepository;
+import com.arc.sbtest.exception.ResourceNotFoundException;
 
 @RestController
 @RequestMapping("/api")
 public class MScontroller 
 {
+	@Autowired
+	PersonRepository personRepository;
+	
 	private static final Logger logger = LogManager.getLogger(MScontroller.class);
+	
+	@GetMapping("/persons")
+	public List<Person> getAllPersons() 
+	{
+		return  personRepository.findAll();
+	}
+	
+	@PostMapping("/persons")
+	public Person createPerson(@Valid @RequestBody Person person)
+	{
+		return personRepository.save(person);		
+	}
+	 
+	@GetMapping("/persons/{id}")
+	public Person getPersonById(@PathVariable(value = "id") Long personId)
+	{
+		return personRepository.findById(personId).orElseThrow(() -> new ResourceNotFoundException("Person", "id", personId));
+	}
+	
+	@PutMapping("/persons/{id}")
+	public Person updatePerson(@PathVariable(value = "id") Long personId,@Valid @RequestBody Person personeDetails)
+	{
+		Person person = personRepository.findById(personId).orElseThrow(() -> new ResourceNotFoundException("Person", "id", personId));
+		
+		person.setName(personeDetails.getName());
+
+		return personRepository.save(person);		
+	}
 	
 	@GetMapping("/generic-hello")
 	public ResponseEntity<String> noReqestParameter() 
@@ -22,7 +65,7 @@ public class MScontroller
 		HttpHeaders responseHeaders = new HttpHeaders();
 		responseHeaders.set("CustomResponseHeader1", "CustomResponseHeaderValue1");
 		responseHeaders.set("Custom-ResponseHeader-2", "Custom-Response-Header-Value-2");
-		return new ResponseEntity<String>("Welcome to generic greetings", responseHeaders, HttpStatus.OK);
+		return new ResponseEntity<>("Welcome to generic greetings", responseHeaders, HttpStatus.OK);
 	}
 	
 	@GetMapping("/personalized-hello")
@@ -40,6 +83,6 @@ public class MScontroller
         logger.error("Oops! We have an Error. OK");
         logger.fatal("Damn! Fatal error. Please fix me.");
         
-		return new ResponseEntity<String>("logging", HttpStatus.OK);
+		return new ResponseEntity<>("logging", HttpStatus.OK);
 	}
 }
