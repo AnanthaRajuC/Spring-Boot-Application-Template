aae89801fca1470891aecdcd3ea90f5af29efe6c
Anantha Raju C <arcswdev@gmail.com>
2019-10-26 19:23:28 +0530

    REMOVED log files
    REMOVED log files

--- src/main/java/com/arc/sbtest/SBtemplateApplication.java
+++ src/main/java/com/arc/sbtest/SBtemplateApplication.java
@@ -3,6 +3,7 @@ package com.arc.sbtest;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import com.arc.sbtest.model.BuiltWith;
 @SpringBootApplication
 @EnableJpaAuditing
 @ComponentScan({"com.arc.sbtest","com.arc.sbtest.controller"})
+@EnableCaching
 public class SBtemplateApplication implements CommandLineRunner
 {
 	@Autowired
