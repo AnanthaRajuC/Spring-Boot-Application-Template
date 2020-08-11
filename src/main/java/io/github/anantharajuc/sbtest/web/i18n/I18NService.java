019bd42b8be5f768714625192f8195747c91c937
Anantha Raju C <arcswdev@gmail.com>
2020-08-11 20:18:53 +0530

    ADDED I18NConfig

---
+++ src/main/java/io/github/anantharajuc/sbtest/web/i18n/I18NService.java
@@ -0,0 +1,28 @@
+package io.github.anantharajuc.sbtest.web.i18n;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class I18NService 
+{
+	@Autowired
+	private MessageSource messageSource;
+	
+	public String getMessage(String messageId)
+	{
+		System.out.println("messageId "+messageId );
+		Locale locale = LocaleContextHolder.getLocale();
+		System.out.println("locale "+locale );
+		return getMessage(messageId, locale); 
+	}
+	
+	public String getMessage(String messageId, Locale locale)
+	{
+		return messageSource.getMessage(messageId, null, locale);
+	}
+}
