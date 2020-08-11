1ea9031544b60107eacf8cdd80ae1d4fdbe6655b
Anantha Raju C <arcswdev@gmail.com>
2020-08-11 12:33:37 +0530

    ADDED DEV, PRODUCTION, QA, STAGING profiles

--- src/main/resources/data/mysql/migrations/V0_0_1__initialize_structure.sql
+++ src/main/resources/data/mysql/migrations/V0_0_1__initialize_structure.sql
@@ -4,6 +4,20 @@
  * Description: Initial database structure
  */
 
+/*
+-- create schema
+CREATE SCHEMA [schema_name];
+
+-- use schema
+USE [schema_name];
+
+-- Create user 
+create user 'USERNAME'@'IPADDRESS' identified by 'PASSWORD';
+
+-- Grant privileges to user
+grant all privileges on *.* to 'USERNAME'@'IPADDRESS' with grant option;
+ */
+
  /*
  * Structure
  */
