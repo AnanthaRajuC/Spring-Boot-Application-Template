ee1e427a59ca8131b603be25134c578daab33848
Anantha Raju C <arcswdev@gmail.com>
2021-05-14 21:41:06 +0530

    FIXED: UI .env file issues

--- src/main/java/io/github/anantharajuc/sbat/web/controllers/SpringBootApplicationTemplateController.java
+++ src/main/java/io/github/anantharajuc/sbat/web/controllers/SpringBootApplicationTemplateController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -156,10 +157,11 @@ public class SpringBootApplicationTemplateController
 	}
 	
 	@GetMapping("/listPersons")
+	//@PreAuthorize("#username == authentication.principal.username")
 	public String listPersons(Model model, @RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size)
 	{
 		final int currentPage = page.orElse(1);
-        final int pageSize = size.orElse(1000);
+        final int pageSize = size.orElse(10);
         
         Page<Person> personPage = personQueryServiceImpl.findPaginated(PageRequest.of(currentPage - 1, pageSize));
         
